93c3e165b0d13f58f759c4dcc8bbe6b89d539e4d
qcc <2606702591@qq.com>
2024-03-08 16:41:10 +0800

    third commit

--- spzx-service-client/service-user-client/src/main/java/com/qcc/spzx/feign/user/UserFeignClient.java
+++ spzx-service-client/service-user-client/src/main/java/com/qcc/spzx/feign/user/UserFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @ClassName: UserFeignClient
- * @Description: 远程服务调用的的客户端
+ * @Description: 远程服务调用的的的客户端
  * @Date 2024/2/5 20:39
  * @Author quchenxi
  * @Version 1.0
